180e7c56a347bebd48c099cddc8d74343de57d5b
jeffwise26 <jeffwise26@gmail.com>
2024-11-21 11:02:35 -0500

    basic accumulate and reduce

--- functional-clojure/src/functional_clojure/core.clj
+++ functional-clojure/src/functional_clojure/core.clj
@@ -1,7 +1,24 @@
 (ns functional-clojure.core
   (:gen-class))
 
+(defn accum [inList]
+  (if (empty? inList) 0
+    (+ (first inList)
+       (accum (rest inList)))))
+
+(defn accumReduce [inList]
+;;  function, initial value, collection
+  (reduce (fn [acc x] (+ acc x)) 0 inList)
+)
+
+(defn subReduce [inList]
+  (reduce (fn [acc x] (- acc x)) 0 inList)
+  )
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (accum [1,2]))
+  (println (accumReduce [1,2]))
+  (println (subReduce [1,2])))
+
+

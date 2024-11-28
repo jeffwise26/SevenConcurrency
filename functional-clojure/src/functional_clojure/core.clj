fde45cda7cbacc030fb6211bc0bd7e6e27362fe5
jeffwise26 <jeffwise26@gmail.com>
2024-11-28 11:03:32 -0500

    clojure-day-2

--- functional-clojure/src/functional_clojure/core.clj
+++ functional-clojure/src/functional_clojure/core.clj
@@ -1,8 +1,9 @@
 (ns functional-clojure.core
   (:gen-class)
   (:require [clojure.core.reducers :as r])
-  (:require [functional-clojure.wikicounter :as w]))
-
+  (:require [functional-clojure.wikicounter :as w])
+  (:require [clojure.core.protocols :refer [CollReduce coll-reduce]]
+  [clojure.core.reducers :refer [CollFold coll-fold]]))
 
 ;; 1 doesn't quite do tail recursion
 ;; 2 clojure doens't have tail recursion anyways
@@ -15,8 +16,8 @@
 (defn accum-loop [inList]
   (loop [list inList acc 0]
     (if (empty? list)
-    acc
-    (recur (rest list) (+ (first list) acc)))))
+      acc
+      (recur (rest list) (+ (first list) acc)))))
 
 (defn accumReduce [inList]
 ;;  function, initial value, collection
@@ -28,6 +29,10 @@
 (defn parallel-sum [numbers]
   (r/fold + numbers))
 
+(defn my-reduce
+  ([f coll] (coll-reduce coll f))
+  ([f init coll] (coll-reduce coll f init)))
+
 (defn -main
   [& args]
   (println (accum [1,2]))
@@ -35,5 +40,6 @@
   (println (subReduce [1,2]))
   (println (parallel-sum [1,2]))
   (println (w/count-words)))
+
 
 
